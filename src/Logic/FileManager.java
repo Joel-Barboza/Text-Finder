40025bcc29b9b96700dc3d21155c674fa880eba1
Joel-Barboza <joelbp5631@gmail.com>
2024-04-17 01:06:26 -0600

    filter file types, windows appearance for file chooser, some comments on code

--- src/Logic/FileManager.java
+++ src/Logic/FileManager.java
@@ -1,24 +1,43 @@
 package Logic;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
 
 public class FileManager {
     public FileManager() {
+
+        // changes the default swing filechooser appeareance to the system one
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
                  UnsupportedLookAndFeelException e) {
             throw new RuntimeException(e);
         }
-        JFileChooser jFileChooser = new JFileChooser();
-        jFileChooser.setCurrentDirectory(new File("."));
-        //jFileChooser.showSaveDialog(null);
-        jFileChooser.setDialogTitle("choosertitle");
-        jFileChooser.setMultiSelectionEnabled(true);
-        jFileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+
+        JFileChooser jFileChooser = new JFileChooser(); // create file chooser
+
+        // create and config file filter
+        FileNameExtensionFilter textDocFilter = new FileNameExtensionFilter("Todos los documentos de texto (*.txt, *.pdf, *.docx)", "txt", "pdf", "docx");
+        FileNameExtensionFilter txtFilter = new FileNameExtensionFilter("Texto sin formato (*.txt)", "txt");
+        FileNameExtensionFilter pdfFilter = new FileNameExtensionFilter("PDF (*.pdf)", "pdf");
+        FileNameExtensionFilter docxFilter = new FileNameExtensionFilter("Documento de Word (*.docx)", "docx");
+
+        jFileChooser.addChoosableFileFilter(textDocFilter);
+        jFileChooser.addChoosableFileFilter(txtFilter);
+        jFileChooser.addChoosableFileFilter(pdfFilter);
+        jFileChooser.addChoosableFileFilter(docxFilter);
+        jFileChooser.setFileFilter(textDocFilter);
+
+
+        jFileChooser.setCurrentDirectory(new File(".")); // set current directory
+
+        jFileChooser.setDialogTitle("Seleccionar archivos"); // file chooser window title
+        jFileChooser.setMultiSelectionEnabled(true); // enable the selection of multiple files/directories at once
+        jFileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES); // allow to select files and directories
         jFileChooser.setAcceptAllFileFilterUsed(false);
-        //int text = jFileChooser.showOpenDialog(null);
+
+        // do something on open button on file chooser window click
         int showOpenDialog = jFileChooser.showOpenDialog(null);
         if (showOpenDialog == JFileChooser.APPROVE_OPTION) {
             File[] uploadDir = jFileChooser.getSelectedFiles();
@@ -26,7 +45,6 @@ public class FileManager {
                 System.out.println(f.getAbsolutePath());
 
             }
-            //return null;
         }
     }
 }
